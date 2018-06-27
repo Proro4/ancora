9c8bdc34aeda44077554346a393dff07959d62d2
Яценко Віталій <yatsenko.v@newway.com.ua>
2018-06-27 18:21:35 +0300

    history & collections page add

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -7,16 +7,47 @@ $(document).ready(function(){
     });
     $('.lang__active').on('click',function () {
         if($(this).closest('.lang').hasClass('open')){
-            $(this).closest('.lang').removeClass('open')
+            $(this).closest('.lang').removeClass('open');
         }else{
-            $(this).closest('.lang').addClass('open')
+            $(this).closest('.lang').addClass('open');
         }
-    })
+    });
     $(window).scroll(function () {
         if($(this).scrollTop() >= 40){
             $('.header-scroll').addClass('active');
+            $('.header-mob-menu').removeClass('mob-d');
         }else{
             $('.header-scroll').removeClass('active');
+            $('.header-mob-menu').addClass('mob-d');
+        }
+    });
+//    MENU OPEN
+    $('.header__menu__but').on('click',function(e){
+        e.preventDefault();
+        if($('.header-mob-menu').hasClass('open')){
+            $('.header-mob-menu,.header__menu__but').removeClass('open');
+        }else{
+            $('.header-mob-menu,.header__menu__but').addClass('open');
+        }
+    });
+    if($('body').width() <= 992){
+        $('.footer').prepend($('.footer__info__list'));
+        $('.footer').prepend($('.footer__contacts'));
+        $('.footer').prepend($('.footer__info__soc'));
+    }else{
+        $('.footer__info').prepend($('.footer__info__list'));
+        $('.footer__info').prepend($('.footer__info__soc'));
+        $('.footer').prepend($('.footer__menu'));
+    }
+    $(window).resize(function () {
+        if($('body').width() <= 992){
+            $('.footer').prepend($('.footer__info__list'));
+            $('.footer').prepend($('.footer__contacts'));
+            $('.footer').prepend($('.footer__info__soc'));
+        }else{
+            $('.footer__info').prepend($('.footer__info__list'));
+            $('.footer__info').prepend($('.footer__info__soc'));
+            $('.footer').prepend($('.footer__menu'));
         }
     });
 });
