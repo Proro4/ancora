02d77dad4ccfd4aa8e8234d5faa9c127d76e7bd4
Яценко Віталій <yatsenko.v@newway.com.ua>
2018-06-27 11:10:34 +0300

    resize main

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -5,4 +5,18 @@ $(document).ready(function(){
         dots:false,
         navText:[,]
     });
+    $('.lang__active').on('click',function () {
+        if($(this).closest('.lang').hasClass('open')){
+            $(this).closest('.lang').removeClass('open')
+        }else{
+            $(this).closest('.lang').addClass('open')
+        }
+    })
+    $(window).scroll(function () {
+        if($(this).scrollTop() >= 40){
+            $('.header-scroll').addClass('active');
+        }else{
+            $('.header-scroll').removeClass('active');
+        }
+    });
 });
